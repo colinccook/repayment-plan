fc1565177220693ce31c048afc0d5e0cba2dbe96
colinccook <colinccook@gmail.com>
2020-02-17 21:22:37 +0000

    some work on programatically calculating the charges and not from a collection

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
-import Hello from './Hello';
-import './style.css';
-import moment from 'moment';
+import React, { Component } from "react";
+import { render } from "react-dom";
+import Hello from "./Hello";
+import "./style.css";
+import moment from "moment";
 
 Date.prototype.addDays = function(days) {
-    var date = new Date(this.valueOf());
-    date.setDate(date.getDate() + days);
-    return date;
-}
+  var date = new Date(this.valueOf());
+  date.setDate(date.getDate() + days);
+  return date;
+};
 
 function getDates(startDate, stopDate, step) {
-    var dateArray = new Array();
-    var currentDate = startDate;
-    while (currentDate <= stopDate) {
-        dateArray.push(new Date (currentDate));
-        currentDate = currentDate.addDays(step);
-    }
-    return dateArray;
+  var dateArray = new Array();
+  var currentDate = startDate;
+  while (currentDate <= stopDate) {
+    dateArray.push(new Date(currentDate));
+    currentDate = currentDate.addDays(step);
+  }
+  return dateArray;
 }
 
 class App extends Component {
@@ -25,89 +25,99 @@ class App extends Component {
     super();
     this.state = {
       now: moment(),
-      then: moment().add(2, 'M'),
-      lastMonday: moment().startOf('isoweek'),
-      chargeFrequency: 'weekly',
+      then: moment().add(2, "M"),
+      lastMonday: moment().startOf("isoweek"),
+      chargeFrequency: null,
       chargeAmount: 100,
       currentBalance: 100,
       payments: {},
-      charges: {},
       totalPayment: 0
     };
   }
 
-  handleChangeChargeFrequency = (event) => {
-    console.log(event.target.value);
-  }
+  handleChangeChargeFrequency = event => {
+    this.setState({ chargeFrequency: event.target.value});
+  };
 
-  handleChangeChargeAmount = (event) => {
-    this.setState({chargeAmount: event.target.value})
-  }
+  handleChangeChargeAmount = event => {
+    this.setState({ chargeAmount: event.target.value });
+  };
 
-  handleChangeCurrentBalance = (event) => {
-    this.setState({currentBalance: event.target.value})
-  }
+  handleChangeCurrentBalance = event => {
+    this.setState({ currentBalance: event.target.value });
+  };
 
-  showMoreWeeks = (event) => {
-    this.setState({then: this.state.then.add(1, 'M')})
-  }
+  showMoreWeeks = event => {
+    this.setState({ then: this.state.then.add(1, "M") });
+  };
 
-  handleChangeCharge = (event) => {
-    var v = +event.target.value;
-    var c = this.state.charges;
-    c[event.target.id] = v;
-
-    if (!event.target.value)
-      delete c[event.target.id]
-    
-    this.setState({charges: c});
-  }
-
-  handleChangePayment = (event) => {
+  handleChangePayment = event => {
     var v = +event.target.value;
     var p = this.state.payments;
     p[event.target.id] = v;
 
-    if (!event.target.value)
-      delete p[event.target.id]
-    
-    const totalPayment = Object.values(p).reduce((t, n) => t + n, 0)
+    if (!event.target.value) delete p[event.target.id];
 
-    this.setState({payments: p, totalPayment: totalPayment});
-  }
+    const totalPayment = Object.values(p).reduce((t, n) => t + n, 0);
+
+    this.setState({ payments: p, totalPayment: totalPayment });
+  };
 
   drawChargeCell(day, index) {
-    return <td>
-      <input type="text" id={day.getTime()} onChange={this.handleChangeCharge} disabled={day < moment()} 
-      tabindex={index} />
-    </td>
+    return (
+      <td>
+        <input
+          type="text"
+          value={this.getCharges(day, moment(day).add('d', 7))}
+          onChange={this.handleChangeCharge}
+          disabled={day < moment()}
+          tabindex={index}
+        />
+      </td>
+    );
   }
 
   drawPaymentCell(day, index) {
-    return <td>
-      <input type="text" id={day.getTime()} onChange={this.handleChangePayment} disabled={day < moment()} 
-      tabindex={index}/>
-    </td>
+    return (
+      <td>
+        <input
+          type="text"
+          id={day.getTime()}
+          onChange={this.handleChangePayment}
+          disabled={day < moment()}
+          tabindex={index}
+        />
+      </td>
+    );
   }
 
   getPayments(day) {
     return Object.keys(this.state.payments)
       .filter(x => x <= day)
       .map(x => this.state.payments[x])
-      .reduce((t, n) => t + n, 0)
+      .reduce((t, n) => t + n, 0);
   }
 
-  getCharges(day) {
-    return Object.keys(this.state.charges)
-      .filter(x => x <= day)
-      .map(x => this.state.charges[x])
-      .reduce((t, n) => t + n, 0)
+  getCharge(day) {
+    if (this.state.chargeFrequency == "weekly") {
+      return moment(day).weekday() === 0 ? 
+      this.state.chargeAmount : 0;
+    } else if (this.state.chargeFrequency == "monthly") {
+      return moment(day) == moment(day).startOf('month') ?
+        this.state.chargeAmount : 0;
+    }
+  }
+
+  getCharges(from, to) {
+    return getDates(from, to, 1)
+      .map(x => this.getCharge(x))
+      .reduce((t, n) => t + n, 0);
   }
 
   getBalance(day) {
-    return +this.state.currentBalance
-      +this.getCharges(day)
-      -this.getPayments(day)
+    return (
+      +this.state.currentBalance + this.getCharges(day) - this.getPayments(day)
+    );
   }
 
   getLastPaymentDay() {
@@ -117,9 +127,11 @@ class App extends Component {
   }
 
   drawBalanceCell(day, index) {
-    return <td>
-      <input type="text" value={this.getBalance(day)} tabindex={index}/>
-    </td>
+    return (
+      <td>
+        <input type="text" value={this.getBalance(day)} tabindex={index} />
+      </td>
+    );
   }
 
   render() {
@@ -130,17 +142,17 @@ class App extends Component {
           <div>
             <label>
               Charge Weekly
-              <input 
-                type="radio" 
-                value="weekly" 
+              <input
+                type="radio"
+                value="weekly"
                 name="charge"
                 onClick={this.handleChangeChargeFrequency}
               />
             </label>
             <label>
               Charge Monthly
-              <input 
-                type="radio" 
+              <input
+                type="radio"
                 value="monthly"
                 name="charge"
                 onClick={this.handleChangeChargeFrequency}
@@ -150,13 +162,21 @@ class App extends Component {
           <div>
             <label>
               Charge Amount:
-              <input type="text" value={this.state.chargeAmount} onChange={this.handleChangeChargeAmount}/>
+              <input
+                type="text"
+                value={this.state.chargeAmount}
+                onChange={this.handleChangeChargeAmount}
+              />
             </label>
           </div>
           <div>
             <label>
               Current Balance:
-              <input type="text" value={this.state.currentBalance} onChange={this.handleChangeCurrentBalance}/>
+              <input
+                type="text"
+                value={this.state.currentBalance}
+                onChange={this.handleChangeCurrentBalance}
+              />
             </label>
           </div>
         </div>
@@ -175,36 +195,55 @@ class App extends Component {
               <th>S</th>
               <th>£B</th>
             </tr>
-            {
-              getDates(this.state.lastMonday.toDate(), this.state.then.toDate(), 7).map(
-                (o, i) => 
-                <tr>
-                  <td>{moment(o).format('DD/MM/YYYY')}</td>
-                  {this.drawChargeCell(o, i)}
-                  {this.drawPaymentCell(o, i+100)}
-                  {this.drawPaymentCell(o.addDays(1), i + 1000)}
-                  {this.drawPaymentCell(o.addDays(2), i + 100000)}
-                  {this.drawPaymentCell(o.addDays(3), i + 1000000)}
-                  {this.drawPaymentCell(o.addDays(4), i + 10000000)}
-                  {this.drawPaymentCell(o.addDays(5), i + 100000000)}
-                  {this.drawPaymentCell(o.addDays(6), i + 1000000000)}
-                  {this.drawBalanceCell(o.addDays(6))}
-                </tr>
-              )
-            }
+            {getDates(
+              this.state.lastMonday.toDate(),
+              this.state.then.toDate(),
+              7
+            ).map((o, i) => (
+              <tr>
+                <td>{moment(o).format("DD/MM/YYYY")}</td>
+                {this.drawChargeCell(o, i)}
+                {this.drawPaymentCell(o, i + 100)}
+                {this.drawPaymentCell(o.addDays(1), i + 1000)}
+                {this.drawPaymentCell(o.addDays(2), i + 100000)}
+                {this.drawPaymentCell(o.addDays(3), i + 1000000)}
+                {this.drawPaymentCell(o.addDays(4), i + 10000000)}
+                {this.drawPaymentCell(o.addDays(5), i + 100000000)}
+                {this.drawPaymentCell(o.addDays(6), i + 1000000000)}
+                {this.drawBalanceCell(o.addDays(6))}
+              </tr>
+            ))}
           </table>
-          <input type="button" value="+" onClick={this.showMoreWeeks}/>
+          <input type="button" value="+" onClick={this.showMoreWeeks} />
         </div>
 
         <div id="summary">
-          {this.getBalance(this.getLastPaymentDay()) > 0 && <h3>You have a balance of £{this.getBalance(this.getLastPaymentDay())} remaining</h3>}
-          {this.getBalance(this.getLastPaymentDay()) <= 0 && <h3>You have paid back the full £{this.state.currentBalance}</h3>}
-          {this.getBalance(this.getLastPaymentDay()) <= 0 && <h3>It will take {Object.keys(this.state.payments).length} payments, with the final being {moment(new Date(this.getLastPaymentDay())).format()}</h3>}
-          {this.getBalance(this.getLastPaymentDay()) < 0 && <h3>This is inclusive of an advance of £{Math.abs(this.getBalance(this.getLastPaymentDay()))} </h3>}
+          {this.getBalance(this.getLastPaymentDay()) > 0 && (
+            <h3>
+              You have a balance of £{this.getBalance(this.getLastPaymentDay())}{" "}
+              remaining
+            </h3>
+          )}
+          {this.getBalance(this.getLastPaymentDay()) <= 0 && (
+            <h3>You have paid back the full £{this.state.currentBalance}</h3>
+          )}
+          {this.getBalance(this.getLastPaymentDay()) <= 0 && (
+            <h3>
+              It will take {Object.keys(this.state.payments).length} payments,
+              with the final being{" "}
+              {moment(new Date(this.getLastPaymentDay())).format()}
+            </h3>
+          )}
+          {this.getBalance(this.getLastPaymentDay()) < 0 && (
+            <h3>
+              This is inclusive of an advance of £
+              {Math.abs(this.getBalance(this.getLastPaymentDay()))}{" "}
+            </h3>
+          )}
         </div>
       </div>
     );
   }
 }
 
-render(<App />, document.getElementById('root'));
+render(<App />, document.getElementById("root"));
